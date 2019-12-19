4e345c81a44e97802befceb953b2ebb868e82411
mathiaslauritsen <mathias.m.lauritsen@gmail.com>
2019-12-19 02:22:38 +0100

    small clean up of day 1

--- day1.py
+++ day1.py
@@ -3,8 +3,6 @@ sum = 0
 for line in test.readlines():
     mass = int(line)
     while mass//3-2 > 0:
-        print(f"mass: {mass}")
         sum += mass//3-2
-        print(f"sum in loop: {sum}")
         mass = mass//3-2
-print(f"sum: {sum}")
+print(f"result: {sum}")
