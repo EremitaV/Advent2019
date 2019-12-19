40bbf28d240811067bdd093a272b8d2e6ab1d98c
mathiaslauritsen <mathias.m.lauritsen@gmail.com>
2019-12-19 02:34:49 +0100

    added starter code for day3

---
+++ day3.py
@@ -0,0 +1,7 @@
+input = open("input3.txt").readlines()
+#real data
+wire1 = input[0].strip().split(",")
+wire2 = input[1].strip().split(",")
+#test data
+wire1 = ['R8','U5','L5','D3']
+wire2 = ['U7','R6','D4','L4']
